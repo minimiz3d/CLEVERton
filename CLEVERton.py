7e95990510c770ed606d93c3d323e32634e5a754
minimiz3d <arthur-357@hotmail.com>
2017-03-30 14:30:09 -0300

    KnowledgeBase

--- CLEVERton.py
+++ CLEVERton.py
@@ -1,11 +1,11 @@
-import pyAIML
+import aiml
 import time
 
 # The Kernel object is the public interface to the AIML interpreter.
-k = pyAIML.Kernel()
+k = aiml.Kernel()
 
 # Use the 'learn' method to load the contents of an AIML file into the Kernel.
-k.learn("startup.xml")
+k.learn("KnowledgeBase/startup.xml")
 
 # Use the 'respond' method to compute the response
 # to a user's input sring. respond() returns the
@@ -16,18 +16,16 @@ k.respond("load aiml cn")
 username = "User"     # "User" by default
 k.setPredicate("TalkerName", username)
 
-
 # Loop forever, reading user input from the command
 # line and printing respones
-while True: 
+while True:
     input = raw_input(username + ": ")
 
     if ("date" or "time") in input:
         curr_time = str(time.ctime())
         k.setPredicate("date", curr_time)
-    
+
     print "CLEVERton: " + k.respond(input)
 
     if "name" in input:
         username = k.getPredicate("TalkerName")
-
