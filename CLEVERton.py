311c20aaa1fc3deefd6b0f87957b0bef2ec74782
guichristmann <guichristmann@gmail.com>
2017-04-11 14:05:08 -0300

    updated kbs

--- CLEVERton.py
+++ CLEVERton.py
@@ -1,10 +1,26 @@
 import aiml
 import time
+import signal
+
+TIMEOUT = 3 # number of seconds to wait before another user input
 
 # this is used for colored output
 class colors:
     USER = '\033[92m'
     CLEVERTON = '\033[94m'
+
+# called when user didn't input anything for some time
+def timedout(signum, frame):
+    print "interrupted"
+
+signal.signal(signal.SIGALRM, timedout)
+
+def user_input():
+    try:
+        input = raw_input(colors.USER + username + ": ")
+        return input
+    except:
+        return "timedout"
 
 # The Kernel object is the public interface to the AIML interpreter.
 k = aiml.Kernel()
@@ -24,13 +40,20 @@ k.setPredicate("TalkerName", username)
 # Loop forever, reading user input from the command
 # line and printing respones
 while True:
+    #signal.alarm(TIMEOUT) # sets time limit for answer
     input = raw_input(colors.USER + username + ": ")
+    time.sleep(0.2)     # a little bit of delay for responses makes for a more comfortable conversation
+    #signal.alarm(0)     # disable alarm after getting input
 
-    if ("date" or "time") in input:
-        curr_time = str(time.ctime())
-        k.setPredicate("date", curr_time)
+    if "date" or "day" in input:
+        curr_date = str(time.ctime()[0:11])
+        k.setPredicate("date", curr_date)   # get current date
+
+    if "time" in input:
+        curr_time = str(time.ctime()[11:19])    # get only current hour
+        k.setPredicate("time", curr_time)
 
     print colors.CLEVERTON + "CLEVERton: " + k.respond(input)
 
-    if "name" in input:
+    if "name" or "im" in input:
         username = k.getPredicate("TalkerName")
